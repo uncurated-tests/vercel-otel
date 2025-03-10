c719250b0dbb0cb0fde5606f4e00fd92e9df096a
agadzik <andrew.gadzik@vercel.com>
2025-03-10 09:25:02 -0400

    upgrade to the latest @vercel/otel package

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,66 +1,10 @@
 import { registerOTel } from '@vercel/otel'
-import { SimpleSpanProcessor, SpanExporter } from '@opentelemetry/sdk-trace-base'
-import { createExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
-import { SpanContext, TraceFlags } from "@opentelemetry/api";
-import { IExportTraceServiceRequest } from "@opentelemetry/otlp-transformer";
-import { trace as tracing } from "@opentelemetry/api";
-
-interface Reader {
-  get: () => {
-    telemetry: TelemetryContext;
-  };
-}
-
-interface TelemetryContext {
-  rootSpanContext: SpanContext;
-  reportSpans: (data: IExportTraceServiceRequest) => void;
-}
-
-const symbol = Symbol.for("@vercel/request-context");
-const requestContext = () =>
-  (globalThis as { [symbol]?: Reader })[symbol]?.get();
 
 export function register() {
-  const spanExporter: SpanExporter = {
-    export: (spans, resultCallback) => {
-      // Converts a Span to IResourceSpans > IScopeSpans > ISpan structure, which
-      // is OTLP format. It's can be directly serialized to JSON or converted
-      // to Protobuf.
-      const data = createExportTraceServiceRequest(spans, {
-        // Uses hex-encoding trace and span IDs. Otherwise, base64 is used.
-        useHex: true,
-        // Uses `{high, low}` format for timestamps. Otherwise, `unixNanon` is used.
-        // TODO Fix this
-        useLongBits: false,
-      });
-
-      const context = requestContext();
-      context?.telemetry.reportSpans(data);
-      // console.log("[OTEL]", JSON.stringify(data));
-      resultCallback({ code: 0, error: undefined });
-    },
-    shutdown: () => {
-      return Promise.resolve();
-    },
-  }
-
-  const spanProcessor = new SimpleSpanProcessor(spanExporter);
-
   registerOTel({
     serviceName: 'vercel-otel-example',
-    spanProcessors: ["auto", spanProcessor],
-    propagators: [{
-      inject: () => undefined,
-      fields: () => [], extract(context) {
-        const { rootSpanContext } = requestContext()?.telemetry ?? {};
-        return rootSpanContext
-          ? tracing.setSpanContext(context, {
-            ...rootSpanContext,
-            isRemote: true,
-            traceFlags: TraceFlags.SAMPLED,
-          })
-          : context;
-      },
-    }]
+    experimental: {
+      exportViaVercelRuntime: true,
+    }
   })
 }
